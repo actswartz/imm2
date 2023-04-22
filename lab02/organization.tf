6dc88796cea4c146d336614902221dd78407137a
actswartz <j@nr4.com>
2023-04-22 17:00:20 -0400

    Update organization.tf

--- lab02/organization.tf
+++ lab02/organization.tf
@@ -1,17 +1,18 @@
+#Get the Organization Moid by Organization Name
+data "intersight_organization_organization" "organization_moid" {
+  name = var.organization_name
+}
+output "organization_moid" {
+  value = data.intersight_organization_organization.organization_moid.results[0].moid
+}
 variable "target_organization" {
   description = "Deployment organization for policies - Terraform deployed"
   type        = string
 }
 
-data "intersight_organization_organization" "target" {
-    name = var.target_organization
-}
-
-output "org_target_moid" {
-    value = data.intersight_organization_organization.target.results[0].moid
-}
 
 # Local vars for easy reference
 locals {
-  target_org_moid = data.intersight_organization_organization.target.results[0].moid
+  target_org_moid = data.intersight_organization_organization.organization_moid.results[0].moid
+  
 }
