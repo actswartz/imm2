99b1fe665f6ca6f71aadd150051cb38daa16f01f
actswartz <j@nr4.com>
2023-04-22 13:22:10 -0400

    Update config_ntp_policy.tf

--- lab01/config_ntp_policy.tf
+++ lab01/config_ntp_policy.tf
@@ -6,8 +6,8 @@ output "organization_moid" {
   value = data.intersight_organization_organization.organization_moid.results[0].moid
 }
 # NTP Policy details
-resource "intersight_ntp_policy" "PODX" {
-  name        = "${var.organization_name}-ntp-policy"
+resource "intersight_ntp_policy" "NTP" {
+  name        = "ntp-policy"
   description = "NTP policy from Terraform"
   enabled     = true
   ntp_servers = [
