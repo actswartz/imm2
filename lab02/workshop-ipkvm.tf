3f6ad1738aed3183ed5c6d69778dcd05f22096c7
actswartz <j@nr4.com>
2023-04-22 16:56:15 -0400

    Update workshop-ipkvm.tf

--- lab02/workshop-ipkvm.tf
+++ lab02/workshop-ipkvm.tf
@@ -1,5 +1,5 @@
 # IP-KVM
-resource "intersight_kvm_policy" "XXX-kvm_enabled_insecure" {
+resource "intersight_kvm_policy" "kvm_enabled_insecure" {
   organization {
     object_type = "organization.Organization"
     moid        = local.target_org_moid
